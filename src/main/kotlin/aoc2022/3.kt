bc81557e23aae9f40fd6024cc603158320471a97
Charles Flynn <charles.flynn@skybettingandgaming.com>
2022-12-03 10:52:29 +0000

    day 3

---
+++ src/main/kotlin/aoc2022/3.kt
@@ -0,0 +1,26 @@
+package aoc2022
+
+private val input = readResourceFileAsLines("3.txt")
+
+fun main() {
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
+}
+
+private fun part1() =
+    input.sumOf { backpack ->
+        backpack.splitIntoCompartments().findCommonItem().toPriority()
+    }
+
+private fun part2() =
+    input.chunked(3).sumOf { groupOfBackpacks ->
+        groupOfBackpacks.findCommonItem().toPriority()
+    }
+
+fun String.splitIntoCompartments(): List<String> =
+    this.chunked(this.length / 2)
+
+fun List<String>.findCommonItem() =
+    this.map(CharSequence::toSet).reduce { acc, chars -> acc.intersect(chars) }.first()
+
+fun Char.toPriority() = if (this.isUpperCase()) this.code - 38 else this.code - 96
