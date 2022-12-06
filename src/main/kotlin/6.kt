eea41e8481478be161f401242a3dcb491e12483e
Charles Flynn <charles.flynn@skybettingandgaming.com>
2022-12-06 09:58:50 +0000

    day 6

---
+++ src/main/kotlin/6.kt
@@ -0,0 +1,15 @@
+private val input = readFile("6.txt")
+
+fun main() {
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
+}
+
+private fun part1() =  findPositionOfDistinctChars(4)
+
+private fun part2() = findPositionOfDistinctChars(14)
+
+private fun findPositionOfDistinctChars(n: Int) =
+    input
+        .windowed(n)
+        .indexOfFirst { it.toSet().size == n } + n
